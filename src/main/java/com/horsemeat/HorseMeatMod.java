7d0083b05c468264b6fc68e9735c95072bf197fe
Daniel Roberts <danielroberts@Daniels-MacBook-Pro-Black.local>
2026-03-14 18:55:57 +0100

    Update to Forge 61.1.3 and fix mod loading system

--- src/main/java/com/horsemeat/HorseMeatMod.java
+++ src/main/java/com/horsemeat/HorseMeatMod.java
@@ -1,13 +1,16 @@
 package com.horsemeat;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(HorseMeatMod.MOD_ID)
 public class HorseMeatMod {
     public static final String MOD_ID = "horsemeat";
     
     public HorseMeatMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(new ModEvents());
         System.out.println("Horse Meat Mod loaded! Extra meat drops from horses!");
     }

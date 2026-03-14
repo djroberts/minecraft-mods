4b2b0207f56023deb1a17bd168a890c3591803a8
Daniel Roberts <danielroberts@Daniels-MacBook-Pro-Black.local>
2026-03-14 19:16:45 +0100

    Fix SubscribeEvent import - remove .api from package

--- src/main/java/com/horsemeat/ModEvents.java
+++ src/main/java/com/horsemeat/ModEvents.java
@@ -6,7 +6,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.SubscribeEvent;
 
 import net.minecraft.util.RandomSource;
 

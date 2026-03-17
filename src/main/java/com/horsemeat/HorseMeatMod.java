83e1d2e62eeaacb2a411e6dc61d2fd79fc79d85e
Daniel Roberts <danielroberts@Daniels-MacBook-Pro-Black.local>
2026-03-17 19:00:52 +0100

    correct category

--- src/main/java/com/horsemeat/HorseMeatMod.java
+++ src/main/java/com/horsemeat/HorseMeatMod.java
@@ -3,16 +3,26 @@ package com.horsemeat;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraft.world.item.CreativeModeTabs;
 
 @Mod(HorseMeatMod.MOD_ID)
 public class HorseMeatMod {
     public static final String MOD_ID = "horsemeat";
-    
+
     public HorseMeatMod(IEventBus modEventBus) {
         ModItems.register(modEventBus);
         NeoForge.EVENT_BUS.register(ModEvents.class);
+        modEventBus.addListener(this::addCreativeTabItems);
         System.out.println("Horse Meat Mod loaded! Extra meat drops from horses!");
+    }
+
+    private void addCreativeTabItems(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.RAW_HORSE_MEAT);
+            event.accept(ModItems.HORSE_STEAK);
+            event.accept(ModItems.HORSE_SAUSAGE);
+        }
     }
 }
